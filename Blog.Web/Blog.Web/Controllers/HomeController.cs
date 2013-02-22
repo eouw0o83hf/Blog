93a166f3db877ef001ab78dc83ecb96ce0e0ad9e
Nathan <nathanlandis@gmail.com>
2013-02-22 13:40:21 -0600

    Blog solution

---
+++ Blog.Web/Blog.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Web.ViewModels.Home;
+
+namespace Blog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Main()
+        {
+            return View(new MainViewModel());
+        }
+    }
+}
---
+++ Blog.Web/Blog.Web/ViewModels/Home/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Web.ViewModels.Home
+{
+    public class MainViewModel
+    {
+    }
+}
